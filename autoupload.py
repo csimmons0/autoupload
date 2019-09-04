ff8ba45ed23dfd4ad5a2418bf89415cb5922d682
Christopher Simmons <csimmons@Christophers-MacBook-Pro.local>
2019-09-03 22:54:47 -0700

    enable uploading folders

--- autoupload.py
+++ autoupload.py
@@ -6,15 +6,112 @@ import os
 import pathlib
 import shutil
 import sys
-from typing import Set
+from typing import Optional, Set
 
-from pydrive.apiattr import ApiResource, ApiResourceList
+from pydrive.apiattr import ApiResourceList
 from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
+from pydrive.drive import GoogleDrive, GoogleDriveFile
 
 DRIVE_VIDEOS_DIR_NAME = "Videos"
+DRIVE_FOLDER_MIME_TYPE = 'application/vnd.google-apps.folder'
 
 logger = logging.getLogger(__package__)
+
+
+def get_drive_dir_id(
+        drive: GoogleDrive,
+        parent_id: Optional[str],
+        dirname: str
+) -> Optional[str]:
+    actual_parent_id = parent_id
+    if parent_id is None:
+        actual_parent_id = "root"
+
+    matches: ApiResourceList = drive.ListFile(
+        {
+            "q": f"'{actual_parent_id}' in parents and title='{dirname}' and "
+                 f"trashed=false"}
+    ).GetList()
+    if len(matches) == 0:
+        return None
+
+    if len(matches) > 1:
+        raise RuntimeError(
+            f"Query for parent_id={actual_parent_id}, title={dirname} resulted in a "
+            f"list of size {len(matches)}"
+        )
+
+    return matches[0]["id"]
+
+
+def make_drive_dir(drive: GoogleDrive, parent_id: str, dirname: str) -> GoogleDriveFile:
+    logger.debug(f"Making drive directory. parent_id={parent_id} dirname={dirname}")
+    drive_dir = drive.CreateFile(dict(
+        title=dirname,
+        parents=[parent_descriptor(parent_id)],
+        mimeType=DRIVE_FOLDER_MIME_TYPE
+    ))
+
+    drive_dir.Upload()
+    return drive_dir
+
+
+def make_drive_videos_subdir(drive: GoogleDrive, relative_path: pathlib.Path):
+    assert not relative_path.is_absolute()
+
+    logger.debug(f"Making videos subdirectory {relative_path}")
+
+    # Get the ID of the Drive videos directory.
+    parent_id = get_drive_dir_id(drive, None, DRIVE_VIDEOS_DIR_NAME)
+    if not parent_id:
+        raise RuntimeError(
+            f"Root videos dir named {DRIVE_VIDEOS_DIR_NAME} does not exist")
+
+    # Iterate over each subdirectory in the relative path.
+    for subdir in relative_path.parts:
+        # Get the folders that already exist in Drive.
+        logger.debug(f"Making subdirectory {subdir}")
+        drive_subdirs = drive.ListFile(
+            {
+                'q': f"'{parent_id}' in parents and trashed=false and "
+                     f"mimeType='{DRIVE_FOLDER_MIME_TYPE}'"
+            }
+        ).GetList()
+
+        # Get the existing Drive folder that has the same name as the
+        # subdirectory that is part of the relative path.
+        drive_subdir = next(
+            (drive_subdir for drive_subdir in drive_subdirs
+             if drive_subdir['title'] == subdir),
+            None
+        )
+
+        # If there is not an existing Drive folder that has the same
+        # name as the subdirectory, create such a Drive folder.
+        if not drive_subdir:
+            drive_subdir = make_drive_dir(drive, parent_id, subdir)
+
+        # Move into the Drive folder that matches this iteration's
+        # subdirectory.
+        parent_id = drive_subdir["id"]
+
+    return parent_id
+
+
+def parent_descriptor(parent_id):
+    return dict(kind="drive#fileLink", id=parent_id)
+
+
+def should_skip_directory(directory: pathlib.Path) -> bool:
+    return is_dotfile(directory)
+
+
+def should_skip_file(filepath: pathlib.Path) -> bool:
+    return is_dotfile(filepath)
+
+
+def is_dotfile(path: pathlib.Path) -> bool:
+    return path.name[0] == "."
 
 
 def main():
@@ -38,47 +135,60 @@ def main():
     gauth.LocalWebserverAuth()
     drive = GoogleDrive(gauth)
 
-    # Get the videos dir in Drive.
-    matches: ApiResourceList = drive.ListFile(
-        {"q": "'root' in parents and title='{}'".format(DRIVE_VIDEOS_DIR_NAME)}
-    ).GetList()
-    if len(matches) != 1:
-        raise RuntimeError(
-            "Query for videos dir resulted in a list of size {}".format(len(matches))
-        )
-
-    drive_videos_dir: ApiResource = matches[0]
-    drive_videos_dir_id = drive_videos_dir["id"]
-
-    # Get the titles of the videos that are already in Drive.
-    drive_videos: ApiResourceList = drive.ListFile(
-        dict(q="'{}' in parents and trashed=false".format(drive_videos_dir_id))
-    ).GetList()
-    drive_video_titles: Set[str] = set(video["title"] for video in drive_videos)
-    logger.debug("drive_video_titles=%s", drive_video_titles)
-
-    # Upload the local videos that are not already in Drive.
+    # Walk the local videos folder.
     local_videos_dir = pathlib.Path(args.local_videos_path)
-    for video_path in local_videos_dir.iterdir():
-        if video_path.is_dir() or video_path.name in drive_video_titles:
-            logger.info("Skipping %s", video_path)
-            continue
+    for directory, subdirs, filenames in os.walk(local_videos_dir):
+        subdirs = list(map(pathlib.Path, subdirs))
+        # Filter out subdirectories that this routine should skip.
+        subdirs[:] = [subdir for subdir in subdirs if not should_skip_directory(subdir)]
 
-        logger.info("Uploading %s", video_path.name)
-        drive_video = drive.CreateFile(
-            dict(
-                title=video_path.name, parents=[parent_descriptor(drive_videos_dir_id)]
-            )
+        # Create the directory in Drive.
+        directory_relative_path = pathlib.Path(directory).relative_to(local_videos_dir)
+        drive_dir_id = make_drive_videos_subdir(drive, directory_relative_path)
+
+        # Create the uploaded files directory.
+        uploaded_files_dir = local_uploaded_videos_path.joinpath(directory_relative_path)
+        uploaded_files_dir.mkdir(parents=True, exist_ok=True)
+
+        # Get the names of the files that are already in Drive.
+        query = \
+            f"'{drive_dir_id}' in parents and mimeType!='{DRIVE_FOLDER_MIME_TYPE}' " \
+            f"and trashed=false "
+
+        logger.debug(f"Getting existing files with query={query}")
+        drive_files: ApiResourceList = drive.ListFile(dict(q=query)).GetList()
+        drive_filenames: Set[str] = set(
+            drive_file["title"] for drive_file in drive_files
         )
-        drive_video.SetContentFile(str(video_path))
-        drive_video.Upload()
-        shutil.move(video_path, local_uploaded_videos_path.joinpath(video_path.name))
+
+        logger.debug("drive_filenames=%s", drive_filenames)
+
+        # Iterate over the files that aren't already in Drive.
+        for filename in set(filenames) - drive_filenames:
+            filepath = pathlib.Path(directory, filename)
+
+            # Skip files that should be skipped.
+            if should_skip_file(filepath):
+                continue
+
+            # Upload the file.
+            logger.info("Uploading %s", filepath.name)
+            drive_file = drive.CreateFile(
+                dict(
+                    title=filepath.name, parents=[parent_descriptor(drive_dir_id)]
+                )
+            )
+
+            drive_file.SetContentFile(str(filepath))
+            drive_file.Upload()
+
+            # Move the file to the uploaded files folder.
+            shutil.move(
+                filepath,
+                uploaded_files_dir.joinpath(filepath.name)
+            )
 
     return 0
-
-
-def parent_descriptor(parent_id):
-    return dict(kind="drive#fileLink", id=parent_id)
 
 
 if __name__ == "__main__":
